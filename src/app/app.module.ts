3331a178cbc0336e6cfaf32aa00594281ed3a206
Rozsi <Rozsi@DESKTOP-G4TJIS0>
2022-04-08 13:45:05 +0200

    Added routing and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { UserInformationComponent } from './pages/user-information/user-information.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        UserProfileComponent,
+        UserInformationComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
