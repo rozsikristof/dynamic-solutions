77eee884b23f4f2c6580e24ae0df64806647cef5
Rozsi <Rozsi@DESKTOP-G4TJIS0>
2022-04-11 22:27:27 +0200

    Added logo redirection

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ds-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    constructor(private readonly router: Router) { }
+
+    redirectToProfilePage(): void {
+        this.router.navigate(['profile']);
+    }
+}
